0088b81b69d17556d44178ba5aec4bd82a0a0fe8
Harishkp77 <shaarini_harish@outlook.com>
2023-08-09 17:15:26 +0530

    no changes as earlier

--- src/Component/Signup.js
+++ src/Component/Signup.js
@@ -1,54 +1,51 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 const Signup = () => {
-  
-    const [formData, setFormData] = useState({
-        name: "",
-        username: "",
-        password: "",
-    });
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    password: "",
+  });
 
-    const [serverFeedback, setServerFeedback] = useState(""); 
+  const [serverFeedback, setServerFeedback] = useState("");
 
-    const handleChange = (e) => {
-        const { id, value } = e.target;
-        setFormData({ ...formData, [id]: value });
-        
-      };
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     fetch("http://localhost:5000/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      })
-        .then((response) => response.text())
-        .then((data) => {
-          setServerFeedback(data);
-
-          setFormData({
-            name: "",
-            username: "",
-            password: "",
-        });
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     })
+      .then((response) => response.text())
+      .then((data) => {
+        setServerFeedback(data);
 
-    .catch((error) => console.error("Error submitting form:", error));
-};
+        setFormData({
+          name: "",
+          username: "",
+          password: "",
+        });
+      })
 
-
+      .catch((error) => console.error("Error submitting form:", error));
+  };
 
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="border p-4 rounded" style={{ maxWidth: '400px' }}>
+      <div className="border p-4 rounded" style={{ maxWidth: "400px" }}>
         <h2 className="mb-4 text-center">Sign Up</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label htmlFor="name" className="form-label">Name</label>
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
             <input
               type="text"
               className="form-control"
@@ -59,7 +56,9 @@ const Signup = () => {
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="username" className="form-label">Username</label>
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
             <input
               type="text"
               className="form-control"
@@ -70,7 +69,9 @@ const Signup = () => {
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password</label>
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
             <input
               type="password"
               className="form-control"
@@ -81,16 +82,21 @@ const Signup = () => {
             />
           </div>
           <div className="d-grid">
-            <button type="submit" className="btn btn-primary" onClick={handleSubmit}>Sign Up</button>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Sign Up
+            </button>
           </div>
           <div className="mt-3 text-center">
-           
             <p>{serverFeedback}</p>
           </div>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default Signup;
